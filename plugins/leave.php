8257ac981f72b55f91f19fa854671d3dd5ffb2cb
#Nick <nicknessuno001@gmail.com>
2018-04-16 21:44:53 +0200

    Create leave.php

---
+++ plugins/leave.php
@@ -0,0 +1,17 @@
+// ESCI DAL GRUPPO IN CUI HAI SCRITTO IL MESSAGGIO
+
+if (0 === strpos($msg, '>leave') and in_array($userID, $admin))
+	{
+	$MadelineProto->messages->sendMessage(['peer' => $chatID, 'message' => "Bye!"]);
+	$MadelineProto->channels->leaveChannel(['channel' => $chatID, ]);
+	}
+
+// ESCE DAL GRUPPO CON CHATID
+// -leave chatid
+
+if (0 === strpos($msg, '-leave ') and in_array($userID, $admin))
+	{
+	$gruppo = str_replace('-leave ', "", $msg);
+	$MadelineProto->channels->leaveChannel(['channel' => $gruppo, ]);
+	$MadelineProto->messages->sendMessage(['peer' => $chatID, 'message' => "Sono uscito da $gruppo>"]);
+	}
